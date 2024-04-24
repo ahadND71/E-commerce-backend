1e70c619628f4bd6ed1ea69d600d3d30c6d7bcea
Mohammad Alkhamis <moh.kha.sa@gmail.com>
2024-04-24 14:12:33 +0300

    added database.sql, which contain all the table creation and data insertion

---
+++ src/models/address.cs
@@ -0,0 +1,7 @@
+namespace MyProductApi.Models
+{
+    public class address
+    {
+        public int address_id { get; set; } // TODO: update
+    }
+}
---
+++ src/models/order.cs
@@ -0,0 +1,7 @@
+namespace MyProductApi.Models
+{
+    public class order
+    {
+        public int order_id { get; set; } // TODO: update
+    }
+}

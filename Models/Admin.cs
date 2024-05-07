8792dbf370b0e5800a38fc7277ae7760c3f4bb61
Enasmbatarfi@gmail.com <enasmbatarfi@gmail.com>
2024-05-07 10:52:17 +0300

    Add unique constraint to the admin table

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -1,5 +1,7 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
+[Index(nameof(Email), IsUnique = true)]
 public class Admin
 {
   public Guid AdminId { get; set; }

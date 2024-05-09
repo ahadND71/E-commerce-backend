100b5a144a202969c78b7c4bebaab199acb0a5b8
ahadND71 <ahadaldossary71@gmail.com>
2024-05-09 19:44:01 +0300

    AutoMapper and Dtos for both admin and customer table has been created

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -20,7 +20,6 @@ public class CustomerController : ControllerBase
     {
         _dbContext = customerService;
         _authService = authService;
-
     }
 
 
@@ -104,131 +103,131 @@ public class CustomerController : ControllerBase
         }
     }
 
-    // [AllowAnonymous]
-    // [HttpPost("login")]
-    // public async Task<IActionResult> LoginCustomer([FromBody] LoginUserDto loginUserDto)
-    // {
-    //     try
-    //     {
-    //         var LoggedCustomer = await _dbContext.LoginCustomerService(loginUserDto);
-    //         if (LoggedCustomer == null)
-    //         {
-    //             return ApiResponse.UnAuthorized("Invalid Credential");
-    //         }
-    //         var token = _authService.GenerateJwtToken(LoggedCustomer);
-    //         return ApiResponse.Success<LoginUserDto>(LoggedCustomer, "Customer is loggedIn successfully", null, token);
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public async Task<IActionResult> LoginCustomer([FromBody] LoginUserDto loginUserDto)
+    {
+        try
+        {
+            var LoggedCustomer = await _dbContext.LoginCustomerService(loginUserDto);
+            if (LoggedCustomer == null)
+            {
+                return ApiResponse.UnAuthorized("Invalid Credential");
+            }
+            var token = _authService.GenerateJwtToken(LoggedCustomer);
+            return ApiResponse.Success<LoginUserDto>(LoggedCustomer, "Customer is loggedIn successfully", null, token);
 
 
-    //     }
-    //     catch (Exception ex)
-    //     {
-    //         Console.WriteLine($"An error occurred, cannot login");
-    //         return ApiResponse.ServerError(ex.Message);
-    //     }
-    // }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred, cannot login");
+            return ApiResponse.ServerError(ex.Message);
+        }
+    }
 
 
-    // [Authorize(Roles = "Admin")]
-    // [HttpPut("{customerId}")]
-    // public async Task<IActionResult> UpdateCustomer(string customerId, Customer updateCustomer)
-    // {
-    //     try
-    //     {
-    //         if (!Guid.TryParse(customerId, out Guid customerIdGuid))
-    //         {
-    //             return ApiResponse.BadRequest("Invalid Customer ID Format");
-    //         }
-    //         var customer = await _dbContext.UpdateCustomerService(customerIdGuid, updateCustomer);
-    //         if (customer == null)
-    //         {
-    //             return ApiResponse.NotFound("No Customer Founded To Update");
-    //         }
-    //         return ApiResponse.Success<Customer>(
-    //             customer,
-    //             "Customer Is Updated Successfully"
-    //         );
-    //     }
-    //     catch (Exception ex)
-    //     {
-    //         Console.WriteLine($"An error occurred, cannot update the Customer ");
-    //         return ApiResponse.ServerError(ex.Message);
+    [Authorize(Roles = "Admin")]
+    [HttpPut("{customerId}")]
+    public async Task<IActionResult> UpdateCustomer(string customerId, Customer updateCustomer)
+    {
+        try
+        {
+            if (!Guid.TryParse(customerId, out Guid customerIdGuid))
+            {
+                return ApiResponse.BadRequest("Invalid Customer ID Format");
+            }
+            var customer = await _dbContext.UpdateCustomerService(customerIdGuid, updateCustomer);
+            if (customer == null)
+            {
+                return ApiResponse.NotFound("No Customer Founded To Update");
+            }
+            return ApiResponse.Success<Customer>(
+                customer,
+                "Customer Is Updated Successfully"
+            );
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred, cannot update the Customer ");
+            return ApiResponse.ServerError(ex.Message);
 
-    //     }
-    // }
+        }
+    }
 
 
-    // [Authorize(Roles = "Admin")]
-    // [HttpDelete("{customerId}")]
-    // public async Task<IActionResult> DeleteCustomer(string customerId)
-    // {
-    //     try
-    //     {
-    //         if (!Guid.TryParse(customerId, out Guid customerIdGuid))
-    //         {
-    //             return ApiResponse.BadRequest("Invalid Customer ID Format");
-    //         }
-    //         var result = await _dbContext.DeleteCustomerService(customerIdGuid);
-    //         if (!result)
-    //         {
-    //             return ApiResponse.NotFound("The Customer is not found to be deleted");
+    [Authorize(Roles = "Admin")]
+    [HttpDelete("{customerId}")]
+    public async Task<IActionResult> DeleteCustomer(string customerId)
+    {
+        try
+        {
+            if (!Guid.TryParse(customerId, out Guid customerIdGuid))
+            {
+                return ApiResponse.BadRequest("Invalid Customer ID Format");
+            }
+            var result = await _dbContext.DeleteCustomerService(customerIdGuid);
+            if (!result)
+            {
+                return ApiResponse.NotFound("The Customer is not found to be deleted");
 
-    //         }
-    //         return ApiResponse.Success(" Customer is deleted successfully");
+            }
+            return ApiResponse.Success(" Customer is deleted successfully");
 
-    //     }
-    //     catch (Exception ex)
-    //     {
-    //         Console.WriteLine($"An error occurred, the Customer can not deleted");
-    //         return ApiResponse.ServerError(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred, the Customer can not deleted");
+            return ApiResponse.ServerError(ex.Message);
 
-    //     }
-    // }
+        }
+    }
 
-    // [AllowAnonymous]
-    // [HttpPost("forgot-password")]
-    // public async Task<IActionResult> ForgotPassword(string email)
-    // {
-    //     try
-    //     {
-    //         var result = await _dbContext.ForgotPasswordService(email);
-    //         if (!result)
-    //         {
-    //             return ApiResponse.NotFound("No user found with this email");
-    //         }
-    //         return ApiResponse.Success("Password reset email sent successfully");
-    //     }
-    //     catch (Exception ex)
-    //     {
-    //         Console.WriteLine($"An error occurred, cannot send password reset link: {ex.Message}");
-    //         return ApiResponse.ServerError(ex.Message);
-    //     }
-    // }
+    [AllowAnonymous]
+    [HttpPost("forgot-password")]
+    public async Task<IActionResult> ForgotPassword(string email)
+    {
+        try
+        {
+            var result = await _dbContext.ForgotPasswordService(email);
+            if (!result)
+            {
+                return ApiResponse.NotFound("No user found with this email");
+            }
+            return ApiResponse.Success("Password reset email sent successfully");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred, cannot send password reset link: {ex.Message}");
+            return ApiResponse.ServerError(ex.Message);
+        }
+    }
 
-    // [AllowAnonymous]
-    // [HttpPost("reset-password")]
-    // public async Task<IActionResult> ResetPassword(ResetPasswordDto resetPasswordDto)
-    // {
-    //     try
-    //     {
-    //         if (resetPasswordDto.NewPassword != resetPasswordDto.ConfirmNewPassword)
-    //         {
-    //             return ApiResponse.BadRequest("Passwords do not match");
-    //         }
+    [AllowAnonymous]
+    [HttpPost("reset-password")]
+    public async Task<IActionResult> ResetPassword(ResetPasswordDto resetPasswordDto)
+    {
+        try
+        {
+            if (resetPasswordDto.NewPassword != resetPasswordDto.ConfirmNewPassword)
+            {
+                return ApiResponse.BadRequest("Passwords do not match");
+            }
 
-    //         var result = await _dbContext.ResetPasswordService(resetPasswordDto);
+            var result = await _dbContext.ResetPasswordService(resetPasswordDto);
 
-    //         if (!result)
-    //         {
-    //             return ApiResponse.BadRequest("User with this email not found");
+            if (!result)
+            {
+                return ApiResponse.BadRequest("User with this email not found");
 
-    //         }
-    //         return ApiResponse.Success("Password reset successfully");
-    //     }
-    //     catch (Exception ex)
-    //     {
-    //         Console.WriteLine($"An error occurred, cannot reset password: {ex.Message}");
-    //         return ApiResponse.ServerError(ex.Message);
-    //     }
-    // }
+            }
+            return ApiResponse.Success("Password reset successfully");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred, cannot reset password: {ex.Message}");
+            return ApiResponse.ServerError(ex.Message);
+        }
+    }
 
 }

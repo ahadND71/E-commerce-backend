bede859d0b34d5a5a46bd70eb5943f70b27204fc
Enasmbatarfi@gmail.com <enasmbatarfi@gmail.com>
2024-05-10 11:02:40 +0300

    add the remaining data for db intialization

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Data;
 
@@ -16,7 +17,6 @@ public class DbInitializer
     {
         context.Database.EnsureCreated();
 
-        //
         // if no admins found add these
         if (!context.Admins.Any())
         {
@@ -266,76 +266,185 @@ public class DbInitializer
             }
         }
 
-        //
-        // orderProduct
-        // if (!context.OrderProducts.Any())
-        // {
-        //     var orderProduct = new OrderProduct[]
-        //     {
-        //         new OrderProduct
-        //         {
-        //             OrderProductId= Guid.Parse(""),
-        //             ProductId = Guid.Parse("7b88a4f8-ee9f-44f7-99ef-e084da0c8ee9"),
-        //             OrderId = Guid.Parse(""),
-        //             ProductPrice = 12,
-        //             Quantity = 2,
-        //         },
-        //
-        //     };
-        //
-        //     foreach (OrderProduct op in orderProduct)
-        //     {
-        //         context.OrderProducts.Add(op);
-        //     }
-        // }
+        // if no order products found add these
+        if (!context.OrderProducts.Any())
+        {
+            var orderProducts = new List<OrderProduct>
+    {
+        // Order 1 Products
+        new OrderProduct
+        {
+            OrderProductId = Guid.Parse("51f1f3a8-23d2-4c80-b159-88a1ee8f893b"),
+            ProductId = Guid.Parse("7b88a4f8-ee9f-44f7-99ef-e084da0c8ee9"),
+            OrderId = Guid.Parse("d043296e-d2d5-4374-88f8-5fe0d6d71e5e"),
+            ProductPrice = 12,
+            Quantity = 2,
+        },
+        new OrderProduct
+        {
+            OrderProductId = Guid.Parse("6727fb21-4ad2-4ad3-8915-8feee0a3f1aa"),
+            ProductId = Guid.Parse("4963d195-33e0-4718-9c0c-f7db68678917"),
+            OrderId = Guid.Parse("d043296e-d2d5-4374-88f8-5fe0d6d71e5e"),
+            ProductPrice = 5.9m,
+            Quantity = 3,
+        },
+        // Order 2 Products
+        new OrderProduct
+        {
+            OrderProductId = Guid.Parse("abf95b0e-bdd5-40c1-aa46-1f5e7803d8c7"),
+            ProductId = Guid.Parse("210518cb-b4c4-4d2f-9a53-f6520b534657"),
+            OrderId = Guid.Parse("f1c9eae7-7ba4-482e-ba5f-f4a795b2c228"),
+            ProductPrice = 25.5m,
+            Quantity = 1,
+        },
+        new OrderProduct
+        {
+            OrderProductId = Guid.Parse("73a0f514-65fb-4667-ba2a-df028fa6d6fd"),
+            ProductId = Guid.Parse("33ad3125-c70f-485e-bf32-b90ad76e3ad4"),
+            OrderId = Guid.Parse("f1c9eae7-7ba4-482e-ba5f-f4a795b2c228"),
+            ProductPrice = 35.5m,
+            Quantity = 2,
+        },
+        new OrderProduct
+        {
+            OrderProductId = Guid.Parse("74b8ed87-7b15-4514-86de-3e39343d35d6"),
+            ProductId = Guid.Parse("b106d590-002f-4b10-9237-b3954651efd0"),
+            OrderId = Guid.Parse("f1c9eae7-7ba4-482e-ba5f-f4a795b2c228"),
+            ProductPrice = 35.5m,
+            Quantity = 1,
+        },
+        // Order 3 Products
+        new OrderProduct
+        {
+            OrderProductId = Guid.Parse("6b05c920-0e45-44e7-af46-dc6dcf332b18"),
+            ProductId = Guid.Parse("7b88a4f8-ee9f-44f7-99ef-e084da0c8ee9"),
+            OrderId = Guid.Parse("b0cd279d-5316-45b5-8c47-fcd3cb707cb2"),
+            ProductPrice = 12m,
+            Quantity = 1,
+        },
+        new OrderProduct
+        {
+            OrderProductId = Guid.Parse("d20a0ae1-2b34-4b60-976e-4ec6a34d393f"),
+            ProductId = Guid.Parse("4963d195-33e0-4718-9c0c-f7db68678917"),
+            OrderId = Guid.Parse("b0cd279d-5316-45b5-8c47-fcd3cb707cb2"),
+            ProductPrice = 5.9m,
+            Quantity = 1,
+        },
+        new OrderProduct
+        {
+            OrderProductId = Guid.Parse("045b27bf-43e9-4d08-b192-67dc4d4d4c65"),
+            ProductId = Guid.Parse("210518cb-b4c4-4d2f-9a53-f6520b534657"),
+            OrderId = Guid.Parse("b0cd279d-5316-45b5-8c47-fcd3cb707cb2"),
+            ProductPrice = 25.5m,
+            Quantity = 2,
+        },
+     };
 
-        //
-        // order
-        // if (!context.Orders.Any())
-        // {
-        //     var order = new Order[]
-        //     {
-        //         new Order
-        //         {
-        //             OrderId = Guid.Parse(""),
-        //             AddressId = Guid.Parse(""),
-        //             CreatedAt = DateTime.UtcNow,
-        //             CustomerId = Guid.Parse(""),
-        //             // OrderProducts = ,
-        //             
-        //         },
-        //
-        //     };
-        //
-        //     foreach (Order o in order)
-        //     {
-        //         context.Orders.Add(o);
-        //     }
-        // }
+            foreach (OrderProduct o in orderProducts)
+            {
+                context.OrderProducts.Add(o);
+            }
+        }
 
+        // if no orders found add these
+        if (!context.Orders.Any())
+        {
+            var orders = new List<Order>
+    {
+        // Order 1
+        new Order
+        {
+            OrderId = Guid.Parse("d043296e-d2d5-4374-88f8-5fe0d6d71e5e"),
+            AddressId = Guid.Parse("cfb3ff24-82db-492e-b7bb-1a02727bc399"),
+            CreatedAt = DateTime.UtcNow,
+            CustomerId = Guid.Parse("feee9ca6-fd69-46cf-a990-64db26780922"),
+        },
+        // Order 2
+        new Order
+        {
+            OrderId = Guid.Parse("f1c9eae7-7ba4-482e-ba5f-f4a795b2c228"),
+            AddressId = Guid.Parse("cfb3ff24-82db-492e-b7bb-1a02727bc399"),
+            CreatedAt = DateTime.UtcNow,
+            CustomerId = Guid.Parse("12295810-446c-4ef3-b5f9-8b7ec0a81e88"),
+        },
+        // Order 3
+        new Order
+        {
+            OrderId = Guid.Parse("b0cd279d-5316-45b5-8c47-fcd3cb707cb2"),
+            AddressId = Guid.Parse("cfb3ff24-82db-492e-b7bb-1a02727bc399"),
+            CreatedAt = DateTime.UtcNow,
+            CustomerId = Guid.Parse("a470781d-df28-4a68-b5f4-9b259b4b69d9"),
+        }
+    };
 
-        //review
-        // if (!context.Reviews.Any())
-        // {
-        //     var review = new Review[]
-        //     {
-        //         new Review
-        //         {
-        //             ReviewId = Guid.Parse(""),
-        //             Comment = "",
-        //             OrderId = Guid.Parse(""),
-        //             CustomerId = Guid.Parse(""),
-        //             ProductId = Guid.Parse(""),
-        //             Rating = 3,
-        //         },
-        //
-        //     };
-        //
-        //     foreach (Review r in review)
-        //     {
-        //         context.Reviews.Add(r);
-        //     }
-        // }
+            foreach (Order o in orders)
+            {
+                context.Orders.Add(o);
+            }
+        }
+
+        // if no reviews found add these
+        if (!context.Reviews.Any())
+        {
+            var reviews = new List<Review>
+    {
+        // Review 1
+        new Review
+        {
+            ReviewId = Guid.Parse("e4f95d92-575f-48da-a001-1f99c7d2d799"),
+            Comment = "Great product!",
+            OrderId = Guid.Parse("d043296e-d2d5-4374-88f8-5fe0d6d71e5e"),
+            CustomerId = Guid.Parse("feee9ca6-fd69-46cf-a990-64db26780922"),
+            ProductId = Guid.Parse("7b88a4f8-ee9f-44f7-99ef-e084da0c8ee9"),
+            Rating = 5,
+        },
+        // Review 2
+        new Review
+        {
+            ReviewId = Guid.Parse("189b91f7-1c57-4684-91cc-5baf7e4d3784"),
+            Comment = "Fast delivery!",
+            OrderId = Guid.Parse("f1c9eae7-7ba4-482e-ba5f-f4a795b2c228"),
+            CustomerId = Guid.Parse("12295810-446c-4ef3-b5f9-8b7ec0a81e88"),
+            ProductId = Guid.Parse("210518cb-b4c4-4d2f-9a53-f6520b534657"),
+            Rating = 4,
+        },
+        // Review 3
+        new Review
+        {
+            ReviewId = Guid.Parse("3c477a6c-ba9c-45d1-bab8-5c656d09de12"),
+            Comment = "Not as described.",
+            OrderId = Guid.Parse("f1c9eae7-7ba4-482e-ba5f-f4a795b2c228"),
+            CustomerId = Guid.Parse("12295810-446c-4ef3-b5f9-8b7ec0a81e88"),
+            ProductId = Guid.Parse("33ad3125-c70f-485e-bf32-b90ad76e3ad4"),
+            Rating = 2,
+        },
+        // Review 4
+        new Review
+        {
+            ReviewId = Guid.Parse("1f63a23a-4f55-4e69-bfe0-23830bdc44b0"),
+            Comment = "Good quality, thank you!",
+            OrderId = Guid.Parse("b0cd279d-5316-45b5-8c47-fcd3cb707cb2"),
+            CustomerId = Guid.Parse("a470781d-df28-4a68-b5f4-9b259b4b69d9"),
+            ProductId = Guid.Parse("7b88a4f8-ee9f-44f7-99ef-e084da0c8ee9"),
+            Rating = 4,
+        },
+        // Review 5
+        new Review
+        {
+            ReviewId = Guid.Parse("8aa7e85a-c9c5-4c92-b6bc-3f401c48d04d"),
+            Comment = "Nice planner stickers!",
+            OrderId = Guid.Parse("b0cd279d-5316-45b5-8c47-fcd3cb707cb2"),
+            CustomerId = Guid.Parse("a470781d-df28-4a68-b5f4-9b259b4b69d9"),
+            ProductId = Guid.Parse("4963d195-33e0-4718-9c0c-f7db68678917"),
+            Rating = 4,
+        },
+    };
+
+            foreach (Review r in reviews)
+            {
+                context.Reviews.Add(r);
+            }
+        }
 
 
         context.SaveChanges();

6d242f570dd72a2594d5145b67ec6378219d09ca
Mohkhasa <moh.kha.sa@gmail.com>
2024-05-11 22:43:09 +0300

    fixed the addresses in DbInitializer.cs

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -131,139 +131,141 @@ public class DbInitializer
                 context.Customers.Add(customer);
             }
         }
+
         //
         // // if no customers found add these
-        // if (!context.Addresses.Any())
-        // {
-        //     var Addresses = new Address[]
-        //     {
-        //         new Address
-        //             {
-        //                 AddressId = Guid.Parse("9d358843-fd84-4af3-b486-fa8725c8af42"),
-        //                 Name = "John Smith",
-        //                 AddressLine1 = "123 Maple Street",
-        //                 AddressLine2 = "Apt 301",
-        //                 Country = "United States",
-        //                 Province = "California",
-        //                 City = "Los Angeles",
-        //                 ZipCode = "90001",
-        //                 CustomerId = Guid.Parse("cb45d6e0-b134-491a-8968-9f27fc5c4c37")
-        //             },
-        //             new Address
-        //             {
-        //                 AddressId = Guid.Parse("76f4d253-2171-4985-8c9e-f1979423ebc8"),
-        //                 Name = "Jane Doe",
-        //                 AddressLine1 = "456 Oak Avenue",
-        //                 AddressLine2 = "",
-        //                 Country = "United States",
-        //                 Province = "New York",
-        //                 City = "New York City",
-        //                 ZipCode = "10001",
-        //                 CustomerId = Guid.Parse("cb45d6e0-b134-491a-8968-9f27fc5c4c37")
-        //             },
-        //             new Address
-        //             {
-        //                 AddressId = Guid.Parse("76f4d253-2171-4985-8c9e-f1979423ebc8"),
-        //                 Name = "David Johnson",
-        //                 AddressLine1 = "789 Elm Street",
-        //                 AddressLine2 = "Suite 102",
-        //                 Country = "United States",
-        //                 Province = "Texas",
-        //                 City = "Houston",
-        //                 ZipCode = "77002",
-        //                 CustomerId = Guid.Parse("a470781d-df28-4a68-b5f4-9b259b4b69d9")
-        //             },
-        //             new Address
-        //             {
-        //                 AddressId = Guid.Parse("fb639447-d196-4de4-b03d-6bcc3ca000e4"),
-        //                 Name = "Emily Williams",
-        //                 AddressLine1 = "321 Pine Avenue",
-        //                 AddressLine2 = "Apt 10B",
-        //                 Country = "United States",
-        //                 Province = "Florida",
-        //                 City = "Miami",
-        //                 ZipCode = "33101",
-        //                 CustomerId = Guid.Parse("a470781d-df28-4a68-b5f4-9b259b4b69d9")
-        //             },
-        //             new Address
-        //             {
-        //                 AddressId = Guid.Parse("c13854e7-aab7-4e5f-88dd-8a701924d80b"),
-        //                 Name = "Michael Brown",
-        //                 AddressLine1 = "567 Cedar Street",
-        //                 AddressLine2 = "Suite 501",
-        //                 Country = "United States",
-        //                 Province = "Illinois",
-        //                 City = "Chicago",
-        //                 ZipCode = "60601",
-        //                 CustomerId = Guid.Parse("12295810-446c-4ef3-b5f9-8b7ec0a81e88")
-        //             },
-        //             new Address
-        //             {
-        //                 AddressId = Guid.Parse("e29e8d72-a949-4fe9-a986-eae03ed2a48f"),
-        //                 Name = "Sarah Wilson",
-        //                 AddressLine1 = "901 Walnut Avenue",
-        //                 AddressLine2 = "",
-        //                 Country = "United States",
-        //                 Province = "Pennsylvania",
-        //                 City = "Philadelphia",
-        //                 ZipCode = "19101",
-        //                 CustomerId = Guid.Parse("12295810-446c-4ef3-b5f9-8b7ec0a81e88")
-        //             },
-        //             new Address
-        //             {
-        //                 AddressId = Guid.Parse("e89df668-cef1-402f-88c2-7c00990867aa"),
-        //                 Name = "Matthew Taylor",
-        //                 AddressLine1 = "234 Birch Street",
-        //                 AddressLine2 = "Apt 15C",
-        //                 Country = "United States",
-        //                 Province = "Massachusetts",
-        //                 City = "Boston",
-        //                 ZipCode = "02101",
-        //                 CustomerId = Guid.Parse("12295810-446c-4ef3-b5f9-8b7ec0a81e88")
-        //             },
-        //             new Address
-        //             {
-        //                 AddressId = Guid.Parse("b931adf8-5ae3-41c0-b6d2-c7da06a90b2b"),
-        //                 Name = "Amanda Martinez",
-        //                 AddressLine1 = "678 Pineapple Avenue",
-        //                 AddressLine2 = "",
-        //                 Country = "United States",
-        //                 Province = "Florida",
-        //                 City = "Orlando",
-        //                 ZipCode = "32801",
-        //                 CustomerId = Guid.Parse("feee9ca6-fd69-46cf-a990-64db26780922")
-        //             },
-        //             new Address
-        //             {
-        //                 AddressId = Guid.Parse("d5c6760b-8563-43e6-950b-2ebdd9ace603"),
-        //                 Name = "Christopher Garcia",
-        //                 AddressLine1 = "890 Cherry Street",
-        //                 AddressLine2 = "Suite 401",
-        //                 Country = "United States",
-        //                 Province = "California",
-        //                 City = "San Francisco",
-        //                 ZipCode = "94101",
-        //                 CustomerId = Guid.Parse("feee9ca6-fd69-46cf-a990-64db26780922")
-        //             },
-        //             new Address
-        //             {
-        //                 AddressId = Guid.Parse("7a5787cd-2d91-430e-a421-9bd60fa072f1"),
-        //                 Name = "Jennifer Rodriguez",
-        //                 AddressLine1 = "543 Plum Avenue",
-        //                 AddressLine2 = "",
-        //                 Country = "United States",
-        //                 Province = "Texas",
-        //                 City = "Austin",
-        //                 ZipCode = "78701",
-        //                 CustomerId = Guid.Parse("feee9ca6-fd69-46cf-a990-64db26780922")
-        //             }
-        //     };
+        if (!context.Addresses.Any())
+        {
+            var addresses = new Address[]
+            {
+                new Address
+                {
+                    AddressId = Guid.Parse("9d358843-fd84-4af3-b486-fa8725c8af42"),
+                    Name = "Home",
+                    AddressLine1 = "123 Maple Street",
+                    AddressLine2 = "Apt 301",
+                    Country = "Saudi Arabia",
+                    Province = "Central",
+                    City = "Riyadh",
+                    ZipCode = "90001",
+                    CustomerId = Guid.Parse("feee9ca6-fd69-46cf-a990-64db26780922")
+                },
+                new Address
+                {
+                    AddressId = Guid.Parse("76f4d253-2171-4985-8c9e-f1979423ebc8"),
+                    Name = "Work",
+                    AddressLine1 = "456 Oak Avenue",
+                    AddressLine2 = "",
+                    Country = "Saudi Arabia",
+                    Province = "Central",
+                    City = "Riyadh",
+                    ZipCode = "10001",
+                    CustomerId = Guid.Parse("feee9ca6-fd69-46cf-a990-64db26780922")
+                },
+                new Address
+                {
+                    AddressId = Guid.Parse("d4b782fb-9f62-4258-9d16-0e673268945e"),
+                    Name = "My house address",
+                    AddressLine1 = "789 Elm Street",
+                    AddressLine2 = "Suite 102",
+                    Country = "Saudi Arabia",
+                    Province = "Eastern",
+                    City = "Dammam",
+                    ZipCode = "77002",
+                    CustomerId = Guid.Parse("a470781d-df28-4a68-b5f4-9b259b4b69d9")
+                },
+                new Address
+                {
+                    AddressId = Guid.Parse("fb639447-d196-4de4-b03d-6bcc3ca000e4"),
+                    Name = "My Work Address",
+                    AddressLine1 = "321 Pine Avenue",
+                    AddressLine2 = "Apt 10B",
+                    Country = "Saudi Arabia",
+                    Province = "Central",
+                    City = "Riyadh",
+                    ZipCode = "33101",
+                    CustomerId = Guid.Parse("a470781d-df28-4a68-b5f4-9b259b4b69d9")
+                },
+                new Address
+                {
+                    AddressId = Guid.Parse("c13854e7-aab7-4e5f-88dd-8a701924d80b"),
+                    Name = "Work",
+                    AddressLine1 = "567 Cedar Street",
+                    AddressLine2 = "Suite 501",
+                    Country = "Saudi Arabia",
+                    Province = "Central",
+                    City = "Riyadh",
+                    ZipCode = "60601",
+                    CustomerId = Guid.Parse("12295810-446c-4ef3-b5f9-8b7ec0a81e88")
+                },
+                new Address
+                {
+                    AddressId = Guid.Parse("e29e8d72-a949-4fe9-a986-eae03ed2a48f"),
+                    Name = "Home",
+                    AddressLine1 = "901 Walnut Avenue",
+                    AddressLine2 = "",
+                    Country = "Saudi Arabia",
+                    Province = "Western",
+                    City = "Jubail",
+                    ZipCode = "19101",
+                    CustomerId = Guid.Parse("12295810-446c-4ef3-b5f9-8b7ec0a81e88")
+                },
+                new Address
+                {
+                    AddressId = Guid.Parse("e89df668-cef1-402f-88c2-7c00990867aa"),
+                    Name = "Friend's house",
+                    AddressLine1 = "234 Birch Street",
+                    AddressLine2 = "Apt 15C",
+                    Country = "Saudi Arabia",
+                    Province = "Western",
+                    City = "Dammam",
+                    ZipCode = "02101",
+                    CustomerId = Guid.Parse("12295810-446c-4ef3-b5f9-8b7ec0a81e88")
+                },
+                new Address
+                {
+                    AddressId = Guid.Parse("b931adf8-5ae3-41c0-b6d2-c7da06a90b2b"),
+                    Name = "Home 1",
+                    AddressLine1 = "678 Pineapple Avenue",
+                    AddressLine2 = "",
+                    Country = "Saudi Arabia",
+                    Province = "Eastern",
+                    City = "Alhasa",
+                    ZipCode = "32801",
+                    CustomerId = Guid.Parse("feee9ca6-fd69-46cf-a990-64db26780922")
+                },
+                new Address
+                {
+                    AddressId = Guid.Parse("d5c6760b-8563-43e6-950b-2ebdd9ace603"),
+                    Name = "Work 2",
+                    AddressLine1 = "890 Cherry Street",
+                    AddressLine2 = "Suite 401",
+                    Country = "Saudi Arabia",
+                    Province = "Eastern",
+                    City = "Khobar",
+                    ZipCode = "94101",
+                    CustomerId = Guid.Parse("feee9ca6-fd69-46cf-a990-64db26780922")
+                },
+                new Address
+                {
+                    AddressId = Guid.Parse("7a5787cd-2d91-430e-a421-9bd60fa072f1"),
+                    Name = "Home 2",
+                    AddressLine1 = "543 Plum Avenue",
+                    AddressLine2 = "",
+                    Country = "Saudi Arabia",
+                    Province = "Eastern",
+                    City = "Dammam",
+                    ZipCode = "78701",
+                    CustomerId = Guid.Parse("feee9ca6-fd69-46cf-a990-64db26780922")
+                }
+            };
 
-        //     foreach (Address address in Addresses)
-        //     {
-        //         context.Addresses.Add(address);
-        //     }
-        // }
+
+            foreach (Address a in addresses)
+            {
+                context.Addresses.Add(a);
+            }
+        }
 
         //
         // if no categories found add these

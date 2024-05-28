e1598eb1b78a154ecf5947fc206090b647c99d0b
ahadND71 <ahadaldossary71@gmail.com>
2024-05-28 05:48:29 +0300

    3001

--- src/Program.cs
+++ src/Program.cs
@@ -46,7 +46,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowOrigin", builder =>
     {
-        builder.WithOrigins("http://localhost:3000")
+        builder.WithOrigins("http://localhost:3001")
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials();
